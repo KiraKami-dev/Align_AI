295f3fd8ff176cbfd05c7326f787ad603af8eeef
KiraKami <akshatkhair41@gmail.com>
2024-01-31 13:21:46 +0530

    inital setup

---
+++ build/app/generated/source/buildConfig/release/com/example/align_ai/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.align_ai;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.align_ai";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
